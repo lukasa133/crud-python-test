eed1f1acd964bfa45109af687c30010a584ad696
lukasa133 <ldiazc3@soy.sena.edu.co>
2026-04-30 09:52:20 -0500

    feat: fase RED, eliminar y consultar

--- tests/test_inventario.py
+++ tests/test_inventario.py
@@ -1,6 +1,7 @@
 #TEST INVENTARIO
 from source.inventario import Inventario
 
+#TEST DE AGREGAR PRODUCTO, DEBE CONFIRMAR SI SE ESTA AGREGANDO
 def test_agregar_producto():
     inventario = Inventario()
 
@@ -10,7 +11,41 @@ def test_agregar_producto():
 
     assert len(inventario.productos) == 1
 
-    
+#TEST ACTUALIZAR PRODUCTO, DEBE VALIDAR SI SE CAMBIO EL VALOR DE ALGUNOS DE LOS ATRIBUTOS
+def test_actualizar_producto():
+    inventario = Inventario()
+
+    inventario.agregar_producto("Mouse", 5)
+
+    inventario.actualizar_producto("Mouse", 10)
+
+    producto = inventario.consultar_producto("Mouse")
+
+
+    assert producto ["cantidad"] == 10
+
+#TEST CONSULTAR PRODUCTO SI ESTA DENTRO DE LA LISTA
+def test_consultar_producto():
+    inventario = Inventario()
+
+    inventario.agregar_producto("Pan", 14)
+
+    producto = inventario.consultar_producto("Pan")
+
+    assert producto["nombre"] == "Pan"
+
+    assert producto["cantidad"] == 14
+
+
+# ELIMINAR PRODUCTO Y VERIFICAR QUE ESTA FUNCIONANDO EL METODO DE ELIMINAR
+def test_eliminar_producto():
+    inventario = Inventario()
+
+    inventario.agregar_producto("Banana", 8)
+
+    inventario.eliminar_producto("Banana")
+
+    assert len(inventario.productos) == 0
 
 
 
