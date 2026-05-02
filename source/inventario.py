06aa1eee464233fef23878bc2a7521e1e727a8f0
lukasa133 <ldiazc3@soy.sena.edu.co>
2026-05-02 13:39:35 -0500

    feat: class inventario.py

--- source/inventario.py
+++ source/inventario.py
@@ -1,3 +1,24 @@
-class Inventario():
-    pass
+class Inventario:
+    def __init__(self):
+        self.productos = []
+
+    def agregar_producto(self, nombre, cantidad):
+        self.productos.append({
+            "nombre": nombre,
+            "cantidad": cantidad
+        })
+    
+    def actualizar_producto(self, nombre, cantidad):
+        producto = self.consultar_producto(nombre)
+        producto["cantidad"] = cantidad
+    
+    def consultar_producto(self, nombre):
+        for producto in self.productos:
+            if producto["nombre"] == nombre:
+                return producto
+        
+    
+    def eliminar_producto(self, nombre):
+        producto = self.consultar_producto(nombre)
+        self.productos.remove(producto)
 
